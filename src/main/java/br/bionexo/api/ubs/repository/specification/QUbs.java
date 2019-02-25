25cfe73ba232387f3eeb7d6122732f0f877f35f5
Andre <alpereir@atech.com.br>
2019-02-25 09:23:45 -0300

    import not used

--- src/main/java/br/bionexo/api/ubs/repository/specification/QUbs.java
+++ src/main/java/br/bionexo/api/ubs/repository/specification/QUbs.java
@@ -4,7 +4,6 @@ import static org.springframework.util.StringUtils.isEmpty;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.springframework.data.jpa.domain.Specification;
